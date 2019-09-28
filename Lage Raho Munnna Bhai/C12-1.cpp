e5a3b4a20a29a794a8033f0f50eaf1d2212d8b98
Samarth Mittal <47500983+the-sam-mit@users.noreply.github.com>
2019-09-28 08:58:25 +0530

    Add files via upload

---
+++ Lage Raho Munnna Bhai/C12-1.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define M 998244853
+
+vector<int> v[112345];
+int vis[112345]={0};
+ll fav[112345], g;
+
+void dfs(int u){
+	vis[u] = 1;
+	g = __gcd(g, fav[u]);
+	for(auto it:v[u]){
+		if(!vis[it]){
+			dfs(it);
+		}
+	}
+}
+
+int main(){
+	int n,m,x,y;
+	cin>>n>>m;
+	for (int i = 1; i <= n; ++i){
+		cin >> fav[i];
+	}
+	for(int i=1; i<=m; i++){
+		cin >> x >> y;
+		v[x].push_back(y);
+		v[y].push_back(x);
+	}
+
+	ll ans = 1;
+	for(int i=1; i<=n; i++){
+		if(!vis[i]){
+			g = fav[i];
+			dfs(i);
+			ans = (ans * g)%M;
+			cout << g << endl;
+		}
+	}
+	cout << ans << endl;
+}
